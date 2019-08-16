c79aeb2d6db87fa31fa6162e53788add0db168e0
Nguyễn Ngọc Hải <52957823+mrjiro9x@users.noreply.github.com>
2019-08-17 03:52:55 +0700

    addingAcc.php create

--- addAccount.php
+++ addAccount.php
@@ -5,26 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="add.php" method="post" enctype="multipart/form-data">
+    <form class="formCreate" action="adddingAcc.php" method="post" enctype="multipart/form-data">
     <div class="create">
         <h1>Create A New Account</h1>
-        <label for="">Full Name</label>
-        <input type="text" name="fullname" id="fullname">
-        <label for="">Username</label>
-        <input type="text" name="username" id="username">
-        <label for="">Password</label>
-        <input type="Password" name="password" id="password">
-        <label for="">Email</label>
-        <input type="text" name="email" id="email">
-        <label for="">Gender</label>
-        <input type="radio" name="gender" id="gender" value="Male">Male
-        <input type="radio" name="gender" id="gender" value="Female">Female
-        <label for="">Avatar</label>
-        <input type="file" name="avatar" id="avatar">
-        <button type="submit" name="submit">Submit</button>
+        <table>
+            <tr>
+                <td>Full Name</td>
+                <td><input type="text" name="fullname" id="fullname" require></td>
+            </tr>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" name="username" id="username" require></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="Password" name="password" id="password" require></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="text" name="email" id="email"></td>
+            </tr>
+            <tr>
+                <td>Gender</td>
+                <td><input type="radio" name="gender" id="gender" value="Male">Male<input type="radio" name="gender" id="gender" value="Female">Female</td>
+            </tr>
+            <tr>
+                <td>Avatar</td>
+                <td><input type="file" name="avatar" id="avatar"></td>
+            </tr>
+        </table>
+        <button type="submit" name="submit">CREATE</button>
     </div>
+    
     </form>
 </body>
 </html>
--- connect.php
+++ connect.php
@@ -2,10 +2,12 @@
 $pdo = new PDO('pgsql:host=ec2-50-19-124-157.compute-1.amazonaws.com;
 port=5432; dbname=dg7lcub6583hf', 'ppiismxltzraqb',
  '5f4cd3ef20b097696768dedf605c1b55e245e3998567e4a94dc654c5e3591aed');
+/*
 if(!pdo){
     echo "Fail!";
 }
 else {
     echo "Success!";
 }
+*/
 ?>
